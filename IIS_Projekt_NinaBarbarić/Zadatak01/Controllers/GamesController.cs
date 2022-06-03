498997b92d7f6ddad664207314194be4678bb602
NinaBarb <61802507+NinaBarb@users.noreply.github.com>
2022-06-03 11:52:38 +0200

    First exercise done

---
+++ IIS_Projekt_NinaBarbarić/Zadatak01/Controllers/GamesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using Repository.Models;
+
+namespace Zadatak01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+
+        [HttpPost]
+        public string Post([FromBody] Game game)
+        {
+            string xml;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Game));
+
+            using (StringWriter textWriter = new Utf8StringWriter())
+            {
+                serializer.Serialize(textWriter, game);
+                xml = textWriter.ToString();
+            };
+
+            var path = Path.GetFullPath("Schemas");
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", path + "\\Game.xsd");
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xml);
+            xmlDocument.Save(path + "\\game.xml");
+            XmlReader rd = XmlReader.Create(path + "\\game.xml");
+            XDocument doc = XDocument.Load(rd);
+            try
+            {
+
+                doc.Validate(schema, ValidationEventHandler);
+            }
+            catch (Exception e)
+            {
+                return "Invalid: " + (e.Message);
+            }
+
+            return xml;
+        }
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            XmlSeverityType type = XmlSeverityType.Warning;
+            if (Enum.TryParse<XmlSeverityType>("Error", out type))
+            {
+                if (type == XmlSeverityType.Error)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+        public class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding => Encoding.UTF8;
+        }
+    }
+}
---
+++ IIS_Projekt_NinaBarbarić/Zadatak07/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zadatak07.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
